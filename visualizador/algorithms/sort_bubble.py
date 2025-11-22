9f14b6962d896544b68c1ca8423df688ec323efe
unknown <gableandrop01@gmail.com>
2025-11-21 22:05:54 -0300

    testeo de commit

---
+++ visualizador/algorithms/sort_bubble.py
@@ -0,0 +1,39 @@
+# Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
+
+items = []
+n = 0
+i = 0
+j = 0
+
+def init(vals):
+    global items, n, i, j
+    items = list(vals)
+    n = len(items)
+    i = 0 #vueltas que dio
+    j = 0 #indice en que esta posicionado
+
+def step():
+    #cuando termina o ya esta ordenada cuando la lista tiene 0 o 1 elemento.
+    global items, n,i,j # global: "Voy a usar y modificar variables que están definidas FUERA de la función"
+    if n<=1:
+        return{"done":True}
+    
+    #comparamos items
+    
+    if items[j]>items[j+1]: #compara la posicion en la que esta con la siguiente
+        items[j],items[j+1]=items[j+1],items[j] #cambio de posicion item j cambia por item j+1
+        resultado={"a": j, "b": j+1, "swap": True, "done": False} #swap el que el intercambio ya se hizo
+    else:
+        resultado={"a": j, "b": j+1, "swap": False, "done": False}
+    j+=1# para que avance al siguiente indice
+    if(j+1 == n-i-0): #j+1 == n-i-2 habia puesto asi pero los ultimos 2 no se ordenaban
+        j=0      
+        i+=1 
+    return resultado
+    # TODO:
+    # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
+    # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
+    # 3) Avanzar punteros (preparar el próximo paso).
+    # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
+    #
+    # Cuando no queden pasos, devolvé {"done": True}.
---
+++ visualizador/algorithms/sort_insertion.py
@@ -0,0 +1,42 @@
+# Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
+
+items = []
+n = 0
+i = 0      # elemento que queremos insertar
+j = None   # cursor de desplazamiento hacia la izquierda (None = empezar)
+
+def init(vals):
+    global items, n, i, j
+    items = list(vals)
+    n = len(items)
+    i = 1      # común: arrancar en el segundo elemento
+    j = None
+
+def step():
+    global items, n,i,j
+    if i>=n:
+        {"done": True}
+    
+    #buscar items 
+    if j is None:
+        j=i
+        return {"a": j, "b": j, "swap": False, "done": False}
+    
+    if j>0 and items[j-1]>items[j]:
+        items[j-1],items[j]=items[j],items[j-1]#swap adyacente= intercambio
+        resultado={"a": j-1, "b": j, "swap": True, "done": False}
+        j=j-1
+        return resultado
+    
+    #cuando no hay mas que desplazar
+    i=i+1
+    j=None
+    return {"a": i-1, "b": i-1, "swap": False, "done": False}
+    
+    
+    # TODO:
+    # - Si i >= n: devolver {"done": True}.
+    # - Si j es None: empezar desplazamiento para el items[i] (p.ej., j = i) y devolver un highlight sin swap.
+    # - Mientras j > 0 y items[j-1] > items[j]: hacer UN swap adyacente (j-1, j) y devolverlo con swap=True.
+    # - Si ya no hay que desplazar: avanzar i y setear j=None.
+
